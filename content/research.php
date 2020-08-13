2a2dc3a46315c93ccc458e66d03c13697ce12874
Pierre-Yves Gousenbourger <pierre-yves.gousenbourger@uclouvain.be>
2020-08-13 13:44:22 +0200

    cleaning the folders

--- content/research.php
+++
@@ -1,9 +0,0 @@
-<div name="Research" id="sec-research" style="display: none" class="tab">
-	<?php include('content/research-fitting.php');?>
-	
-	<?php include('content/research-surfaces.php');?>
-	
-	<?php include('content/research-curves.php');?>
-	
-	<?php include('content/research-groups.php');?>
-</div><!-- research -->
